7327e7776576d05db043ddc988a811b0300ac9ac
동혁 윤 <aaaa6767@outlook.kr>
2023-05-28 23:16:47 +0900

    1189, 13335

---
+++ 1189/1189.cpp
@@ -0,0 +1,53 @@
+﻿// 1189.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int R, C, K;
+char map[6][6];
+bool ch[6][6];
+
+int dx[] = { -1, 1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+int ans = 0;
+
+void dfs(int x, int y, int Dep)
+{
+	if (x == C - 1 && y == 0 && Dep == K)
+	{
+		ans++;
+		return;
+	}
+
+	ch[x][y] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int xx = x + dx[i];
+		int yy = y + dy[i];
+
+		if (xx >= 0 && yy >= 0 && xx < C && yy < R)
+		{
+			if (!ch[xx][yy] && map[xx][yy] != 'T')
+			{
+				dfs(xx, yy, Dep + 1);
+			}
+		}
+	}
+
+	ch[x][y] = false;
+}
+int main()
+{
+	cin >> R >> C >> K;
+
+	for (int i = 0; i < C; i++)
+		for (int j = 0; j < R; j++)
+			cin >> map[i][j];
+
+	dfs(0, R - 1, 1);
+
+	cout << ans;
+}
---
+++ 13335/13335.cpp
@@ -0,0 +1,62 @@
+﻿// 1283.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void Input(int &n, int &w, int &L, queue<int> &truckWeights)
+{
+    cin >> n >> w >> L;
+
+    for (int i = 0; i < n; i++)
+    {
+        int weight;
+        cin >> weight;
+        truckWeights.push(weight);
+    }
+}
+
+void Sol(int n, int w, int L, queue<int> truckWeights)
+{
+    queue<pair<int, int>> bridgeWeight;
+    int time = 0;
+    int totalWeight = 0;
+
+    //트럭이 전부 나가면 멈춤
+    while (!truckWeights.empty() || !bridgeWeight.empty())
+    {
+        time++;
+
+        //다리에 있는 트럭
+        if (!bridgeWeight.empty())
+        {
+            if (time - bridgeWeight.front().second >= w)
+            {
+                totalWeight -= bridgeWeight.front().first;
+                bridgeWeight.pop();
+            }
+        }
+
+        //아직 출발 못한 트럭
+        if (!truckWeights.empty())
+        {
+            if (totalWeight + truckWeights.front() <= L)
+            {
+                totalWeight += truckWeights.front();
+                bridgeWeight.push(make_pair(truckWeights.front(), time));
+                truckWeights.pop();
+            }
+        }
+    }
+
+    cout << time;
+}
+
+int main() {
+    int n, w, L;
+    queue<int> truckWeights;
+  
+    Input(n, w, L, truckWeights);
+    Sol(n, w, L, truckWeights);
+}
