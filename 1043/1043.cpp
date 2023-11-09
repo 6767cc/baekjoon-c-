e2278c53fe690ad36f30a153daabd11edbeed1d6
동혁 윤 <dong0728@hs.ac.kr>
2023-11-09 15:08:55 +0900

    1939 풀기

---
+++ 1043/1043.cpp
@@ -0,0 +1,41 @@
+﻿// 1043.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M, ans;
+int KnowCount;
+
+vector<int> KnowHum;
+vector<int> Party[51];
+bool SayTrue[51];
+
+int main()
+{
+    cin >> N >> M;
+    
+    cin >> KnowCount;
+
+    for (int i = 0; i < KnowCount; i++)
+    {
+        int KnowHuman;
+        cin >> KnowHuman;
+        KnowHum.push_back(KnowHuman);
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        int PartyKnowCount;
+        cin >> PartyKnowCount;
+
+        for (int j = 0; j < PartyKnowCount; j++)
+        {
+            int P;
+            cin >> P;
+            Party[i].push_back(P);
+        }
+    }
+}
---
+++ 1541/1541.cpp
@@ -0,0 +1,63 @@
+﻿// 1541.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string input;
+
+string PlusNumber[51];
+string MinusNumber[51];
+
+double ans = 0;
+
+int main()
+{
+    int i = 0, j = -1;
+    bool M = false;
+
+    cin >> input;
+
+    for (char c : input)
+    {
+        if ((c >= '0' && c <= '9') && !M)
+        {
+            PlusNumber[i] += c;
+        }
+        else if((c >= '0' && c <= '9') && M)
+        {
+            MinusNumber[j] += c;
+        }
+        else if (c == '+' && !M)
+        {
+            i++;
+        }
+        else if (c == '+' && M)
+        {
+            j++;
+        }
+        else if(c == '-')
+        {
+            M = true;
+            j++;
+        }
+    }
+
+    for (int k = 0; k <= i; k++)
+    {
+        double a = stod(PlusNumber[k]);
+
+        ans += a;
+    }
+
+    for (int k = 0; k <= j; k++)
+    {
+        double a = stod(MinusNumber[k]);
+
+        ans -= a;
+    }
+
+    cout << ans;
+}
