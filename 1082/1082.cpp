c83a0844b26a274896b4074faa91d05f2d2dc92b
동혁 윤 <dong0728@hs.ac.kr>
2023-11-18 20:40:16 +0900

    32132

--- 1082/1082.cpp
+++ 1082/1082.cpp
@@ -13,8 +13,12 @@ int P[11];
 vector<pair<int, int>> p;
 string ans;
 
-void input()
+int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N;
 
     for (int i = 0; i < N; i++)
@@ -22,31 +26,36 @@ void input()
         cin >> P[i];
         p.push_back(pair<int, int>(P[i], i));
     }
+
     sort(p.begin(), p.end());
-
     cin >> M;
-}
 
-void MinimumAns()
-{
+
     //숫자가 0 단 하나
-    if (N == 1) ans = "";
+    if (N == 1)
+    {
+        cout << "0";
+        return 0;
+    }
+
     //가장 가격이 싼 숫자가 0일 경우
-    else if (p[0].second == 0)
+    if (p[0].second == 0)
     {
         int m = M - p[1].first;
         if (m < 0)
         {
-            ans = "";
-            return;
+            cout << "0";
+            return 0;
         }
 
         ans += to_string(p[1].second);
-        int cot = m / p[0].first;
+        M -= p[1].first;
+
+        int cot = M / p[0].first;
         for (int i = 0; i < cot; i++)
         {
             ans += to_string(p[0].second);
-            M = m - cot * p[0].first;
+            M = M - p[0].first;
         }
     }
     //가격이 가장 싼 숫자가 0이 아닐경우
@@ -56,17 +65,8 @@ void MinimumAns()
         for (int i = 0; i < cot; i++)
         {
             ans += to_string(p[0].second);
-            M -= cot * p[0].first;
+            M -= p[0].first;
         }
-    }
-}
-
-void AnsOut()
-{
-    if (ans == "")
-    {
-        cout << "0";
-        return;
     }
 
     for (int i = 0; i < ans.size(); i++)
@@ -74,30 +74,20 @@ void AnsOut()
         bool Ch = false;
 
         int Changer = P[ans[i] - '0'];
+
         for (int j = N - 1; j >= 0; j--)
         {
             //남은 돈으로 바꿀 수 있는 숫자면 바꾸기
-            if (M - (P[j] - Changer) >= 0) 
+            if (M - (P[j] - Changer) >= 0)
             {
-                M -= P[j] - Changer;
-                Ch = true;
-                ans[i] = j + '0';
+                    M -= P[j] - Changer;
+                    Ch = true;
+                    ans[i] = j + '0';
             }
             if (Ch) break;
         }
         if (!Ch) break;
     }
+
     cout << ans;
-}
-
-void sol()
-{
-    MinimumAns();
-    AnsOut();
-}
-
-int main()
-{
-    input();
-    sol();
 }
